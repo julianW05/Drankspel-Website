e39b7b663d86c8ff38b399553e650ac356e6bb45
JulianW051 <julianwessels1@gmail.com>
2023-06-15 12:08:04 +0200

    game aanmaken af

--- drunkpoly/src/components/Game_modal.jsx
+++ drunkpoly/src/components/Game_modal.jsx
@@ -1,156 +1,178 @@
-import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import {db, analytics, provider} from '../Firebase-config'
 import React, { useState, useEffect } from "react";
-import { NavLink, Link, Outlet, useNavigate, useParams } from "react-router-dom";
 import Modal from 'react-modal';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { collection, query, where, getDocs, getDoc, addDoc, setDoc, doc } from "firebase/firestore";
-
+import { db, provider } from '../Firebase-config';
+import { NavLink, Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
 const GameModal = () => {
-  const [user, setUser] = useState();
   const [modalIsOpen, setIsOpen] = React.useState(false);
-  const auth = getAuth();
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     gameName: '',
-    player1: '',
-    player2: '',
-    player3: '',
-    player4: ''
-  });
+    playerNumber: '2',
+    playerNames: ['', '']
+  });  
 
-  const authUser = async () => {
-    onAuthStateChanged(auth, (userData) => {
-      if (!userData) {
-        navigate('/');
-      } else if (userData) {
-        setUser(userData);
-      }});
-  };
+  const auth = getAuth();
 
   function openModal() {
     setIsOpen(true);
   }
 
   useEffect(() => {
-    authUser();
-    openModal();
-  }, [])
-
-    const handleInputChange = (event) => {
-      setFormData({
-        ...formData,
-        [event.target.name]: event.target.value
+    const authUser = async () => {
+      onAuthStateChanged(auth, (userData) => {
+        if (!userData) {
+          navigate('/');
+        } else if (userData) {
+          setUser(userData);
+        }
       });
     };
 
-    const handleAddGame = async (event) => {
-      event.preventDefault();
+    authUser();
+    openModal();
+  }, [auth]);
 
-      const { gameName, player1, player2, player3, player4 } = formData;
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+  
+    if (name.startsWith("playerNames[")) {
+      const index = Number(name.match(/\[(\d+)\]/)[1]);
+  
+      setFormData((prevState) => {
+        const updatedPlayerNames = [...prevState.playerNames];
+        updatedPlayerNames[index] = value;
+  
+        return {
+          ...prevState,
+          playerNames: updatedPlayerNames
+        };
+      });
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [name]: value
+      }));
+    }
+  };
+  
+  
 
-      if (!gameName) {
-        alert('Game name is required');
-        return;
-      } else if (!player1) {
-        alert('Speler 1 is vereist');
-        return;
-      } else if (!player2) {
-        alert('Speler 2 is vereist');
-        return;
-      } else if (!player3) {
-        alert('Speler 3 is vereist');
-        return;
-      } else if (!player4) {
-        alert('Speler 4 is vereist');
+  const handleAddGame = async (event) => {
+    event.preventDefault();
+
+    const { gameName, playerNumber, playerNames } = formData;
+
+    if (!gameName) {
+      alert('Spel naam is vereist');
+      return;
+    } else if (!playerNumber) {
+      alert('Speler aantal is vereist');
+      return;
+    } else if (playerNumber < 2) {
+      alert('Minimaal 2 spelers vereist');
+      return;
+    } else if (playerNames.length !== Number(playerNumber) || playerNames.some(name => name === '')) {
+      alert('Vul alle spelers in');
+      return;
+    }
+
+    try {
+      const userDocRef = query(collection(db, "users"), where('uid', '==', user.uid));
+      const userSnapshot = await getDocs(userDocRef);
+
+      if (userSnapshot.empty) {
+        console.error('User document not found');
         return;
       }
 
-      try {
-        // 2. Access the user document and create a new game in the 'games' collection
-        const userDocRef = query(collection(db, "users"), where('uid', '==', user.uid));
-        const userSnapshot = await getDocs(userDocRef);
+      userSnapshot.forEach(async (document) => {
+        const user_db = doc(db, "users", document.id);
+        const gamesCollectionRef = collection(user_db, "games");
+        const gameDocRef = await addDoc(gamesCollectionRef, {
+          gameName: gameName
+        });
 
-        if (userSnapshot.empty) {
-          console.error('User document not found');
-          return;
+        const playersCollectionRef = collection(gameDocRef, "players");
+
+        for (let i = 0; i < playerNumber; i++) {
+          const playerName = playerNames[i];
+          const playerDocRef = doc(playersCollectionRef, `player${i + 1}`);
+          await setDoc(playerDocRef, {
+            name: playerName,
+            position: i + 1
+          });
         }
 
-        userSnapshot.forEach(async (document) => {
-          const user_db = doc(db, "users", document.id);
-          const gamesCollectionRef = collection(user_db, "games");
-          const gameDocRef = await addDoc(gamesCollectionRef, {
-              gameName: gameName
-          });
+        const docSnap = await getDoc(gameDocRef);
+        if (docSnap.exists()) {
+          navigate(`/game/${docSnap.id}`);
+        } else {
+          console.log("No such document!");
+        }
+      });
 
-          const playersCollectionRef = collection(gameDocRef, "players");
+      // Clear the form data
+      setFormData({
+        gameName: '',
+        playerNumber: '2',
+        playerNames: ['', '']
+      });
 
-          const player1DocRef = doc(playersCollectionRef, "player1");
-          await setDoc(player1DocRef, {
-              name: player1,
-              position: 1
-          });
+      console.log('Game created successfully!');
+    } catch (error) {
+      console.error('Error creating game:', error);
+    }
+  };
 
-          const player2DocRef = doc(playersCollectionRef, "player2");
-          await setDoc(player2DocRef, {
-              name: player2,
-              position: 1
-          });
+  const renderPlayerNameInputs = () => {
+    const { playerNumber, playerNames } = formData;
+  
+    return Array.from({ length: Number(playerNumber) }, (_, index) => (
+      <div key={index}>
+        <label>Speler {index + 1}</label>
+        <input
+          type="text"
+          name={`playerNames[${index}]`}
+          value={playerNames[index] || ''}
+          onChange={handleInputChange}
+        />
+      </div>
+    ));
+  };  
 
-          const player3DocRef = doc(playersCollectionRef, "player3");
-          await setDoc(player3DocRef, {
-              name: player3,
-              position: 1
-          });
+  return (
+    <Modal isOpen={modalIsOpen} contentLabel="Example Modal" ariaHideApp={false}>
+      <h2>Maak een nieuw spel</h2>
+      <form onSubmit={handleAddGame}>
+        <div>
+          <label>Spel Naam</label>
+          <input
+            type="text"
+            name="gameName"
+            value={formData.gameName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Speler Aantal</label>
+          <input
+            type="number"
+            name="playerNumber"
+            min="2"
+            max="4"
+            value={formData.playerNumber}
+            onChange={handleInputChange}
+          />
+        </div>
+        {renderPlayerNameInputs()}
+        <button type="submit">Create game</button>
+      </form>
+    </Modal>
+  );
+};
 
-          const player4DocRef = doc(playersCollectionRef, "player4");
-          await setDoc(player4DocRef, {
-              name: player4,
-              position: 1
-          });
-
-          const docSnap = await getDoc(gameDocRef);
-          if (docSnap.exists()) {
-            navigate(`/game/${docSnap.id}`);
-          } else {
-            console.log("No such document!");
-          }
-
-        });
-
-        // Clear the form data
-        setFormData({
-          gameName: '',
-          player1: '',
-          player2: '',
-          player3: '',
-          player4: ''
-        });
-
-        console.log('Game created successfully!');
-      } catch (error) {
-        console.error('Error creating game:', error);
-      }
-    };
-
-      return (
-          <Modal isOpen={modalIsOpen} contentLabel="Example Modal" ariaHideApp={false}>
-              <h2>Maak een nieuw spel</h2>
-              <form className="row" onSubmit={handleAddGame}>
-              {/* Render your form inputs here */}
-              <label className="col-md-11">Spel Naam</label>
-              <input className="col-md-9" type="text" name="gameName" onChange={handleInputChange} value={formData.gameName}/>
-              <label className="col-md-11">Speler 1</label>
-              <input className="col-md-9" type="text" name="player1" onChange={handleInputChange} value={formData.player1}/>
-              <label className="col-md-11">Speler 2</label>
-              <input className="col-md-9" type="text" name="player2" onChange={handleInputChange} value={formData.player2}/>
-              <label className="col-md-11">Speler 3</label>
-              <input className="col-md-9" type="text" name="player3" onChange={handleInputChange} value={formData.player3}/>
-              <label className="col-md-11">Speler 4</label>
-              <input className="col-md-9" type="text" name="player4" onChange={handleInputChange} value={formData.player4}/>
-              <button className="col-md-4" type="submit">Create game</button>
-              </form>
-          </Modal>
-      )
-}
 export default GameModal;
