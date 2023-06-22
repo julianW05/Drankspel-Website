1757bee4d29af9b336f730f2a349ba288030bceb
TheunissenSil <theunissensil@gmail.com>
2023-06-23 00:30:34 +0200

    Final updates

--- drunkpoly/src/layouts/GameLayout.jsx
+++ drunkpoly/src/layouts/GameLayout.jsx
@@ -216,6 +216,10 @@ export default function GameLayout() {
             <div className="blur_container">
                 <div className="legend">
                     <div className="legend_item_container row">
+
+                        <div className="legend_item col-md-9">{gameName}</div>
+                    </div>
+                    <div className="legend_item_container row">
                         <div className="blauw col-md-2"></div>
                         <div className="legend_item col-md-9">{playerNames.player1}</div>
                     </div>
