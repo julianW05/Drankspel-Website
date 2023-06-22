e6f633540fc59a430624759a2ef64f922d8eff67
TheunissenSil <theunissensil@gmail.com>
2023-06-22 21:57:22 +0200

    Update game

--- drunkpoly/src/components/AssignmentModal_modal.jsx
+++ drunkpoly/src/components/AssignmentModal_modal.jsx
@@ -1,22 +1,116 @@
 import React, { useState, useEffect } from "react";
 import Modal from 'react-modal';
+import { collection, query, where, getDocs, addDoc, setDoc, doc, getDoc } from "firebase/firestore";
+import { db } from '../Firebase-config.jsx';
 
-const AssignmentModal = ({ playerTurn, playerNames, playerPositions, onClose, aantalPlayers, setPlayerTurn}) => {
+const AssignmentModal = ({ playerTurn, playerNames, playerPositions, onClose, aantalPlayers, setPlayerTurn, setplayerPrisonRounds, playerPrisonRounds }) => {
     const [assignmentPlayerName, setAssignmentPlayerName] = useState('');
-    const [assignmentPlayerPosition, setAssignmentPlayerPosition] = useState(0);
+    const [ExercieTitle, setExercieTitle] = useState('');
+    const [ExercieDescription, setExercieDescription] = useState('');
 
-    const showExercise = () => {
+    const showExercise = async () => {
         const currentPlayerName = playerNames[`player${playerTurn}`];
         const currentPlayerPosition = playerPositions[`player${playerTurn}`];
+        let currentVakje = '';
+
+        if (currentPlayerPosition == 1) {
+            currentVakje = "start";
+        } else if (currentPlayerPosition == 2 || currentPlayerPosition == 17) {
+            currentVakje = "time to get drunk";
+        } else if (currentPlayerPosition == 3 || currentPlayerPosition == 18 || currentPlayerPosition == 29 || currentPlayerPosition == 34) {
+            currentVakje = "dare";
+        } else if (currentPlayerPosition == 4) {
+            currentVakje = "butler";
+        } else if (currentPlayerPosition == 5 || currentPlayerPosition == 25 || currentPlayerPosition == 33) {
+            currentVakje = "kingsen";
+        } else if (currentPlayerPosition == 6 || currentPlayerPosition == 16 || currentPlayerPosition == 26) {
+            currentVakje = "cheers";
+        } else if (currentPlayerPosition == 7) {
+            currentVakje = "designer baby";
+        } else if (currentPlayerPosition == 8 || currentPlayerPosition == 13 || currentPlayerPosition == 23 || currentPlayerPosition == 37) {
+            currentVakje = "truth";
+        } else if (currentPlayerPosition == 9 || currentPlayerPosition == 32) {
+            currentVakje = "drie man";
+        } else if (currentPlayerPosition == 10 || currentPlayerPosition == 28) {
+            currentVakje = "hoger lager";
+        } else if (currentPlayerPosition == 11 ) {
+            currentVakje = "waterval";
+        } else if (currentPlayerPosition == 12) {
+            currentVakje = "wethouder";
+        } else if (currentPlayerPosition == 14 || currentPlayerPosition == 38) {
+            currentVakje = "rood zwart";
+        } else if (currentPlayerPosition == 15 || currentPlayerPosition == 36) {
+            currentVakje = "juffen";
+        } else if (currentPlayerPosition == 19) {
+            currentVakje = "drinking buddy";
+        } else if (currentPlayerPosition == 20) {
+            currentVakje = "beurt overslaan";
+        } else if (currentPlayerPosition == 21) {
+            currentVakje = "shotje uitdelen";
+        } else if (currentPlayerPosition == 22) {
+            currentVakje = "dierentuin";
+        } else if (currentPlayerPosition == 24) {
+            currentVakje = "opnieuw";
+        } else if (currentPlayerPosition == 27 || currentPlayerPosition == 40) {
+            currentVakje = "rustig aan";
+        } else if (currentPlayerPosition == 30) {
+            currentVakje = "water pauze";
+        } else if (currentPlayerPosition == 31) {
+            currentVakje = "ga naar de gevangenis";
+        } else if (currentPlayerPosition == 35) {
+            currentVakje = "drink master";
+        } else if (currentPlayerPosition == 39) {
+            currentVakje = "shot";
+        }
+
+        // Zet alle informatie goed
+        const vakjesDocRef = query(collection(db, "vakjes"), where('naam', '==', currentVakje));
+        const vakjesSnapshot = await getDocs(vakjesDocRef);
+
+        vakjesSnapshot.forEach(async (document) => {
+            setExercieTitle(document.data().naam);
+            setExercieDescription(document.data().uitleg);
+        }); 
         setAssignmentPlayerName(currentPlayerName);
-        setAssignmentPlayerPosition(currentPlayerPosition);
-        setPlayerTurn((prevState) => {
-            if (prevState === aantalPlayers) {
-                return 1;
+
+        // check of player opnieuw moet gooien
+        if (currentVakje === "opnieuw") {
+            console.log("Je mag nog een keer gooien");
+        } else {
+            // Player turn naar volgende speler
+            setPlayerTurn((prevState) => {
+                if (prevState === aantalPlayers) {
+                    return 1;
+                } else {
+                    return prevState + 1;
+                }
+            });
+        }
+
+        // Moet naar gevangenis
+        if (currentPlayerPosition === 31) {
+            const inPrisonRounds = playerPrisonRounds[`player${playerTurn}`] || 0;
+            if (inPrisonRounds < 3) {
+              console.log("Je zit in de gevangenis");
+              setplayerPrisonRounds((prevPrisonRounds) => ({
+                ...prevPrisonRounds,
+                [`player${playerTurn}`]: inPrisonRounds + 1
+              }));
+              setAssignmentPlayerName(currentPlayerName);
+              return; 
             } else {
-                return prevState + 1;
+              console.log("Je bent vrijgelaten uit de gevangenis!");
+              setplayerPrisonRounds((prevPrisonRounds) => ({
+                ...prevPrisonRounds,
+                [`player${playerTurn}`]: 0
+              }));
             }
-        });
+        }
+
+        // Check of er een extra dobbelsteen gegooid moet worden
+        if (currentVakje === "drie man" || currentVakje === "time to get drunk") {
+            console.log("Je mag nog een keer gooien");
+        }
     };
 
     useEffect(() => {
@@ -26,7 +120,8 @@ const AssignmentModal = ({ playerTurn, playerNames, playerPositions, onClose, aa
     return (
         <Modal isOpen={playerTurn !== null} ariaHideApp={false}>
             <h2>Opdracht van: {assignmentPlayerName}</h2>
-            <p>Positie speler: {assignmentPlayerPosition}</p>
+            <h3>{ExercieTitle}</h3>
+            <p>{ExercieDescription}</p>
             <button onClick={onClose}>Sluiten</button>
         </Modal>
     );
--- drunkpoly/src/components/Dice_modal.jsx
+++ drunkpoly/src/components/Dice_modal.jsx
@@ -10,97 +10,146 @@ const DiceModal = ({ setPlayerTurn, playerPositions, playerTurn, playerNames, se
     const [showResult, setShowResult] = useState(false);
     const [showAssignmentModal, setShowAssignmentModal] = useState(false);
     const [aantalPlayers, setAantalPlayers] = useState(0);
-  
+    const [playerPrisonRounds, setplayerPrisonRounds] = useState({
+        player1: 0,
+        player2: 0,
+        player3: 0,
+        player4: 0
+    });
+
     const openModal = () => {
-      setIsOpen(true);
-      setShowDice(false);
-      setShowResult(false);
+        setIsOpen(true);
+        setShowDice(false);
+        setShowResult(false);
     };
-  
+
     const roleTheDice = () => {
-      setDice(Math.floor(Math.random() * 12) + 1);
-      setShowDice(true);
-      setShowResult(true);
+        if (playerTurn === 2) {
+            setDice(23);
+        } else {
+            setDice(Math.floor(Math.random() * 12) + 1);
+        }
+        setShowDice(true);
+        setShowResult(true);
     };
-  
+
     const handleButtonClick = () => {
-      roleTheDice();
+        const currentPosition = playerPositions[`player${playerTurn}`];
+
+        // Check if in prison. If yes, then don't roll the dice
+        if (currentPosition === 31) {
+            const inPrisonRounds = playerPrisonRounds[`player${playerTurn}`] || 0;
+            if (inPrisonRounds < 3) {
+            setPlayerTurn((prevState) => {
+                const nextPlayerTurn = prevState === aantalPlayers ? 1 : prevState + 1;
+                setplayerPrisonRounds((prevPrisonRounds) => ({
+                ...prevPrisonRounds,
+                [`player${playerTurn}`]: inPrisonRounds + 1
+                }));
+                return nextPlayerTurn;
+            });
+            return;
+            } else {
+            setplayerPrisonRounds((prevPrisonRounds) => ({
+                ...prevPrisonRounds,
+                [`player${playerTurn}`]: 0
+            }));
+            }
+        }
+
+        roleTheDice();
     };
-  
+
     const handleContinue = () => {
-      setIsOpen(false);
-      const updatedPlayerPositions = {
-        ...playerPositions,
-        [`player${playerTurn}`]: playerPositions[`player${playerTurn}`] + dice
-      };
-      setPlayerPositions(updatedPlayerPositions);
-  
-      if (playerPositions.player3 === 0) {
-        setAantalPlayers(2);
-      } else if (playerPositions.player4 === 0) {
-        setAantalPlayers(3);
-      } else {
-        setAantalPlayers(4);
-      }
-    };
-  
-    const handleSeeAssignment = () => {
-      setShowAssignmentModal(true);
-    };
-  
-    const handleCloseAssignmentModal = () => {
-      setShowAssignmentModal(false);
-      setShowResult(false);
-    };
-  
-    useEffect(() => {
-      if (showAssignmentModal) {
         setIsOpen(false);
-      }
+
+        const updatedPlayerPositions = {
+            ...playerPositions,
+            [`player${playerTurn}`]: playerPositions[`player${playerTurn}`] + dice
+        };
+
+        if (updatedPlayerPositions[`player${playerTurn}`] > 40) {
+            updatedPlayerPositions[`player${playerTurn}`] = updatedPlayerPositions[`player${playerTurn}`] - 40;
+        }
+
+        setPlayerPositions(updatedPlayerPositions);
+
+        if (playerPositions.player3 === 0) {
+            setAantalPlayers(2);
+        } else if (playerPositions.player4 === 0) {
+            setAantalPlayers(3);
+        } else {
+            setAantalPlayers(4);
+        }
+    };
+
+    const handleSeeAssignment = () => {
+        setShowAssignmentModal(true);
+    };
+
+    const handleCloseAssignmentModal = () => {
+        setShowAssignmentModal(false);
+        setShowResult(false);
+    };
+
+    useEffect(() => {
+        if (showAssignmentModal) {
+            setIsOpen(false);
+        }
     }, [showAssignmentModal]);
-  
+
     return (
-      <div>
+    <div>
         {showResult && (
-          <button onClick={handleSeeAssignment}>Zie opdracht</button>
+            <button onClick={handleSeeAssignment}>Zie opdracht</button>
         )}
-  
+
         {!showResult && (
-          <button className="" onClick={openModal}>
-            Volgende speler
-          </button>
-        )}
-  
-        <Modal isOpen={modalIsOpen} ariaHideApp={false}>
-          {!showResult && (
-            <h2>{playerNames[`player${playerTurn}`]} Is aan de beurt</h2>
-          )}
-          {showResult && (<h2>Resultaat:</h2>)}
-  
-          {!showResult && (
-            <button onClick={handleButtonClick}>Dobbelen</button>
-          )}
-          {showResult && (<p>{dice}</p>)}
-  
-          {showResult && (
-            <button onClick={handleContinue}>
-              Ga verder
+            <button className="" onClick={openModal}>
+                Volgende speler
             </button>
-          )}
-        </Modal>
-  
-        {showAssignmentModal && (
-          <AssignmentModal
-            playerTurn={playerTurn}
-            playerNames={playerNames}
-            playerPositions={playerPositions}
-            onClose={handleCloseAssignmentModal}
-            aantalPlayers={aantalPlayers}
-            setPlayerTurn={setPlayerTurn}
-          />
         )}
-      </div>
+
+        <Modal isOpen={modalIsOpen} ariaHideApp={false}>
+            {!showResult && (
+                <h2>{playerNames[`player${playerTurn}`]} Is aan de beurt</h2>
+            )}
+            {showResult && (<h2>Resultaat:</h2>)}
+
+            {playerPositions[`player${playerTurn}`] === 31 && (
+                <p>Je zit vast in de gevangenis. Je moet 3 beurten overslaan</p>
+            )}
+            {playerPositions[`player${playerTurn}`] === 31 && (
+                <p>Druk op dobbel om door te gaan!</p>
+            )}
+
+            {!showResult && (
+                <button onClick={handleButtonClick}>Dobbel</button>
+            )}
+
+            {showResult && (<p>{dice}</p>)}
+
+            {showResult && (
+                <button onClick={handleContinue}>
+                Ga verder
+                </button>
+            )}
+        </Modal>
+
+        {showAssignmentModal && (
+            <AssignmentModal
+                playerTurn={playerTurn}
+                playerNames={playerNames}
+                playerPositions={playerPositions}
+                onClose={handleCloseAssignmentModal}
+                aantalPlayers={aantalPlayers}
+                setPlayerTurn={setPlayerTurn}
+                setplayerPrisonRounds={setplayerPrisonRounds}
+                playerPrisonRounds={playerPrisonRounds}
+            />
+        )}
+    </div>
     );
-  };
-  
-  export default DiceModal;
+};
+
+export default DiceModal;
